25389e3d7643ed427463a4cec89e1132ae7ad496
Ali Zain Ul Yaoob <zain@mirage.ar>
2023-09-30 01:33:23 +0500

    Run multiple search phrases concurrently in separate tasks

--- index.ts
+++ index.ts
@@ -10,89 +10,126 @@ import {
 } from "./utils";
 
 const videoFilePath =
-  "/home/ali/Videos/TV Series/The Office/The.Office.US.SEASON.01.S01.COMPLETE.720p.WEBRip.2CH.x265.HEVC-PSA/The.Office.US.S01E01.Pilot.720p.WEBRip.2CH.x265.HEVC-PSA.mkv";
+  "/Users/azuy/Videos/TV Series/Modern Family Season 1-11 Complete 720p x264 [Pahe.in]/Modern Family Season 1 BluRay 720p x264 - Pahe.in/Modern.Family.S01E01.720p.BluRay.x264.150MB-Pahe.in.mkv";
 
-const searchPhrases = ["That's what she said", "Michael Scott"];
+const searchPhrases = ["Oh My God", "Hello"];
 
-const outputDir = "mashups_the_office";
+const outputDir = "mashups_modern_family";
 const clipsDir = "clips";
 
 interface Ctx {
-  searchPhrase: string;
-  subtitleChunks: Cue[];
+  searchPhrases: string[];
+  subtitleChunks: Record<string, Cue[]>;
 }
 
-const tasks = new Listr<Ctx>([
-  {
-    title: "Find phrases in subtitle file",
-    task: async (ctx, task) => {
-      const subtitleChunks = await findPhraseInSubtitleFile(
-        videoFilePath,
-        ctx.searchPhrase
-      );
-      ctx.subtitleChunks = subtitleChunks;
-      task.title = `Found ${subtitleChunks.length} occurrences of ${ctx.searchPhrase}`;
-    },
-  },
-  {
-    title: "Create clips folder",
-    skip: (ctx) => ctx.subtitleChunks.length === 0,
-    task: async (ctx, task) => {
-      createDir(clipsDir);
-    },
-  },
-  {
-    title: "Cut clips",
-    skip: (ctx) => ctx.subtitleChunks.length === 0,
-    task: (ctx, task) =>
-      task.newListr(
-        ctx.subtitleChunks.map((subtitle, index) => ({
-          title: `Cutting clip ${index} from ${formatTimestamp(
-            subtitle.start
-          )} to ${formatTimestamp(subtitle.end)}`,
-          task: async () => {
-            await cutClip(
-              videoFilePath,
-              formatTimestamp(subtitle.start, { format: "WebVTT" }),
-              formatTimestamp(subtitle.end, { format: "WebVTT" }),
-              `${clipsDir}/${encodeFileName(ctx.searchPhrase)}_${index}.mkv`
-            );
-          },
-        })),
-        {
-          concurrent: true,
-          rendererOptions: { collapseSubtasks: false, collapseErrors: false },
-        }
-      ),
-  },
-  {
-    title: "Create output folder",
-    skip: (ctx) => ctx.subtitleChunks.length === 0,
-    task: async () => {
-      createDir(outputDir);
-    },
-  },
-  {
-    title: "Join clips",
-    skip: (ctx) => ctx.subtitleChunks.length === 0,
-    task: async (ctx) => {
-      const fileName = encodeFileName(ctx.searchPhrase);
-      await joinClips(fileName, clipsDir, outputDir);
-    },
-  },
-  {
-    title: "Remove clips",
-    skip: (ctx) => ctx.subtitleChunks.length === 0,
-    task: async () => {
-      await fs.rm("clips", { recursive: true, force: true });
-    },
-  },
-]);
+const allTasks = new Listr<Ctx>({
+  title: `Create Mashup Videos for ${searchPhrases.join(", ")}`,
+  task: (ctx, task) =>
+    task.newListr<Ctx>(
+      ctx.searchPhrases.map((searchPhrase) => ({
+        title: `Creating mashup for ${searchPhrase}`,
+        task: (ctx, subTask) =>
+          subTask.newListr<Ctx>(
+            [
+              {
+                title: "Find phrases in subtitle file",
+                task: async (ctx, task) => {
+                  const subtitleChunks = await findPhraseInSubtitleFile(
+                    videoFilePath,
+                    searchPhrase
+                  );
+                  ctx.subtitleChunks = {
+                    ...ctx.subtitleChunks,
+                    [searchPhrase]: subtitleChunks,
+                  };
+                  task.output = `Found ${subtitleChunks.length} occurrences of ${searchPhrase}`;
+                },
+                rendererOptions: { persistentOutput: true },
+              },
+              {
+                title: "Create clips folder",
+                skip: (ctx) => ctx.subtitleChunks[searchPhrase]?.length === 0,
+                task: async (ctx, task) => {
+                  createDir(clipsDir);
+                },
+                rendererOptions: { persistentOutput: true },
+              },
+              {
+                title: "Cut clips",
+                skip: (ctx) => ctx.subtitleChunks[searchPhrase]?.length === 0,
+                task: (ctx, task) =>
+                  task.newListr(
+                    ctx.subtitleChunks[searchPhrase].map((subtitle, index) => ({
+                      title: `Cutting clip ${index} from ${formatTimestamp(
+                        subtitle.start
+                      )} to ${formatTimestamp(
+                        subtitle.end
+                      )} ${subtitle.text.replace("\n", " ")}`,
+                      task: async () => {
+                        await cutClip(
+                          videoFilePath,
+                          formatTimestamp(subtitle.start, { format: "WebVTT" }),
+                          formatTimestamp(subtitle.end, { format: "WebVTT" }),
+                          `${clipsDir}/${encodeFileName(
+                            searchPhrase
+                          )}_${index}.mkv`
+                        );
+                      },
+                    })),
+                    {
+                      concurrent: true,
+                      exitOnError: true,
+                      rendererOptions: {
+                        collapseSubtasks: false,
+                        collapseErrors: false,
+                      },
+                    }
+                  ),
+              },
+              {
+                title: "Create output folder",
+                skip: (ctx) => ctx.subtitleChunks[searchPhrase]?.length === 0,
+                task: async () => {
+                  createDir(outputDir);
+                },
+              },
+              {
+                title: "Join clips",
+                skip: (ctx) => ctx.subtitleChunks[searchPhrase]?.length === 0,
+                task: async (ctx) => {
+                  const fileName = encodeFileName(searchPhrase);
+                  await joinClips(fileName, clipsDir, outputDir);
+                },
+              },
+              {
+                title: "Remove clips",
+                skip: (ctx) => ctx.subtitleChunks[searchPhrase]?.length === 0,
+                task: async () => {
+                  // await fs.rm("clips", { recursive: true, force: true });
+                  await Promise.all(
+                    ctx.subtitleChunks[searchPhrase].map((subtitle, index) =>
+                      fs.unlink(
+                        `${clipsDir}/${encodeFileName(
+                          searchPhrase
+                        )}_${index}.mkv`
+                      )
+                    )
+                  );
+                },
+              },
+            ],
+            {
+              concurrent: false,
+            }
+          ),
+      })),
+      {
+        concurrent: true,
+        rendererOptions: { collapseSubtasks: false, collapseErrors: false },
+      }
+    ),
+});
 
 console.time("Total time");
-
-for (const searchPhrase of searchPhrases) {
-  await tasks.run({ searchPhrase } as Ctx);
-}
-
+await allTasks.run({ searchPhrases } as Ctx);
 console.timeEnd("Total time");
